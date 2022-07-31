5ab55b8f79096fb697ad86e23ef5ca1844e008a4
EC2 Default User <ec2-user@ip-172-31-43-241.ap-south-1.compute.internal>
2022-07-31 05:40:26 +0000

    initial commit

---
+++ kusuma12/deploy.java
@@ -0,0 +1,30 @@
+
+<!DOCTYPE html>
+<html>
+<title>HTML Tutorial</title>
+<body>
+
+<h1>This is a heading</h1>
+<p>This is a paragraph.</p>
+
+</body>
+</html>
+CSS
+The language for styling web pages
+
+
+
+
+CSS Example:
+body {
+  background-color: lightblue;
+}
+
+h1 {
+  color: white;
+  text-align: center;
+}
+
+p {
+  font-family: verdana;
+}
